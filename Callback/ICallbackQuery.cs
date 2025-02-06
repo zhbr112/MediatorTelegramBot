ff11f8eeb11d7729718b23a062a103935994ed26
zhbr112 <bigrashinboss1@gmail.com>
2025-02-07 02:54:19 +0800

    Удаление лишнего

--- Callback/ICallbackQuery.cs
+++ Callback/ICallbackQuery.cs
@@ -1,12 +1,6 @@
-﻿using MediatorTelegramBot.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
+
 
 namespace MediatorTelegramBot.Callback;
 
