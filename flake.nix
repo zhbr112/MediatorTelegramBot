8a659fb2273580ddc8b31e8840477e6d51aed7e8
zhbr112 <bigrashinboss1@gmail.com>
2025-10-09 06:59:26 +0800

    ы

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
                 authentication = pkgs.lib.mkOverride 10 ''
                     #type database DBuser auth-method
                     local all all trust
-                    host all all trust
+                    host all all 127.0.0.1/32 trust
                 '';
                 enable = true;
                 enableTCPIP = true;
