2eeb58df10feff2163e2e1aa6f0d7be9e32815ad
zhbr112 <bigrashinboss1@gmail.com>
2025-10-09 22:17:24 +0800

    ss

--- flake.nix
+++ flake.nix
@@ -74,6 +74,7 @@
                     local all all trust
                     host all all 127.0.0.1/32 trust
                     host all all ::1/128 trust
+                    host all admin 192.168.0.0/24 scram-sha-256
                 '';
                 enable = true;
                 enableTCPIP = true;
